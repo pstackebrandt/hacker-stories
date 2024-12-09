7a90d85068d73e427a19c748df31734214b27c14
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2024-12-09 12:55:54 +0100

    Use of an data object within component as input. Add family greeting and data object to App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,24 @@
 function App() {
   const daughter_name = "Hannah";
   const daughter_age = 7;
+
+  const family = {
+    father: "Peter",
+    mother: "Stefanie",
+    daughter: "Hannah"    
+  }
   
   return (
     <div>
       <h1>Hello {daughter_name}!</h1>
       <p>You are {daughter_age} years old. Nice!</p>
 
+      <h2>Hello {family.mother} and {family.father}!</h2>
+      <p>Congratulations! Your child is healthy and lively!</p>
+
       <h1>Page purpose</h1>
       <p>Show use of simple constants in component.</p>
+      <p>Use of an object with data.</p>
     </div>
 
   )

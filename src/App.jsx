704eb1d9f952ff1914a36fadbab9f119789ebe40
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2024-12-09 12:46:23 +0100

    Use constants in component Enhance App component with constants for daughter name and age, and add descriptive content

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,18 @@
 // Without Demo code
 
 function App() {
+  const daughter_name = "Hannah";
+  const daughter_age = 7;
+  
   return (
-   <div>
-      <h1>Hello Hannah!</h1>
-   </div>
+    <div>
+      <h1>Hello {daughter_name}!</h1>
+      <p>You are {daughter_age} years old. Nice!</p>
+
+      <h1>Page purpose</h1>
+      <p>Show use of simple constants in component.</p>
+    </div>
+
   )
 }
 

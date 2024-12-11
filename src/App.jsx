785e0639462922af74434a6df7ece099b2a38e21
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2024-12-11 12:30:49 +0100

    Create JSX from objects list by map() Add frameworks and libraries section to App component with mapping functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,45 @@ const welcomeData = {
   title: "Hackers",
 }
 
-const pageDescription = () => ({
-  purpose: "Examples of using simple structures as input for a component.",
-  content: ["Use of object as component input", "Use of arrow function as component input"]
-})
+const frameworksAndLibs = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+  {
+    title: 'Vue.js',
+    url: 'https://vuejs.org/',
+    author: 'Evan You',
+    num_comments: 4,
+    points: 3,
+    objectID: 2,
+  },
+  {
+    title: 'Svelte',
+    url: 'https://svelte.dev/',
+    author: 'Rich Harris',
+    num_comments: 5,
+    points: 5,
+    objectID: 3,
+  },
+];
 
+const pageDescription = () => ({
+  purpose: "Examples of using listed objects as input for a component.",
+  content: ["Use of listed objects as component input", "Create JSX from lists by map()"]
+})
 
 const App = () => {
   return (
@@ -23,7 +57,21 @@ const App = () => {
         <input id="search" type="text" />
       </section>
 
+      <section>
+        <hr />
+        <h2>Frameworks and Libraries</h2>
+        <ul>
+          {frameworksAndLibs.map((tool) =>
+            <li key={tool.objectID}>
+              <label htmlFor={tool.title + "Link"}>{tool.title}: </label>
+              <a id={tool.title + "Link"} href={tool.url}>{tool.url}</a>
+            </li>
+          )}
+        </ul>
+      </section>
+
       <aside>
+        <hr />
         <h2>Page purpose</h2>
         <p>{pageDescription().purpose}</p>
         <ul>{pageDescription().content.map((part, index) =>

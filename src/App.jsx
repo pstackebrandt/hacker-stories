098fbdc1bd2cd1435c9e176e7ed3a7e561c570a5
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2024-12-11 09:59:57 +0100

    Use data from separate function in same file. Add function to retrieve grandparents and update family display

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
 // App.jsx
 // Without Demo code
 
+
+/**
+   * Get the grand parents of daughter Hannah.
+   */
+function getGrandParents() {
+  // It would be more generic to save the parents of each parent separately.
+  // We may search better solutions later.
+  return {
+    parentsOfMother: {
+      father: "Manfred",
+      mother: "Ursula"
+    },
+    parentsOfFather: {
+      father: "Hans",
+      mother: "Ulla"
+    }
+  }
+}
+
+const hannahsInterests = ["playing", "reading", "drawing"];
+
 function App() {
   const daughter_name = "Hannah";
   const daughter_age = 7;
@@ -8,22 +29,29 @@ function App() {
   const family = {
     father: "Peter",
     mother: "Stefanie",
-    daughter: "Hannah"    
+    daughter: "Hannah"
   }
-  
+
+  const fathersParents = getGrandParents().parentsOfFather;
+
   return (
     <div>
       <h1>Hello {daughter_name}!</h1>
       <p>You are {daughter_age} years old. Nice!</p>
 
-      <h2>Hello {family.mother} and {family.father}!</h2>
-      <p>Congratulations! Your child is healthy and lively!</p>
+      <h2>Your parents</h2>
+      <p>{family.mother} and {family.father} like you and are very proud of you.</p>
+
+      <h2>Your grand parents</h2>
+      <p>The grand parents you know are {getGrandParents().parentsOfMother.father},&nbsp;
+        {getGrandParents().parentsOfMother.mother} and {fathersParents.mother}. 
+        Unfortunately, you never got the chance to meet Hans.</p>
 
       <h1>Page purpose</h1>
       <p>Show use of simple constants in component.</p>
       <p>Use of an object with data.</p>
+      <p>Use data from a separate function.</p>
     </div>
-
   )
 }
 

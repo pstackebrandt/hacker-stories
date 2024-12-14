58ad2a3652318de1d97fec9e63fbc8e357134a3c
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2024-12-14 12:34:15 +0100

    Refactor App component to import frameworks and libraries data from a separate data file, removing hardcoded data for improved maintainability.

--- src/App.jsx
+++ src/App.jsx
@@ -4,40 +4,8 @@ import { useState } from 'react';
 // Import config data
 import { welcomeData } from './config/welcome';
 
-const frameworksAndLibs = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-  {
-    title: 'Vue.js',
-    url: 'https://vuejs.org/',
-    author: 'Evan You',
-    num_comments: 4,
-    points: 3,
-    objectID: 2,
-  },
-  {
-    title: 'Svelte',
-    url: 'https://svelte.dev/',
-    author: 'Rich Harris',
-    num_comments: 5,
-    points: 5,
-    objectID: 3,
-  },
-];
+// Import data
+import { frameworksAndLibs } from './data/frameworks';
 
 const pageDescription = () => ({
   purpose: "Examples of using listed objects as input for a component.",
---
+++ src/data/frameworks.js
@@ -0,0 +1,34 @@
+export const frameworksAndLibs = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+    {
+      title: 'Vue.js',
+      url: 'https://vuejs.org/',
+      author: 'Evan You',
+      num_comments: 4,
+      points: 3,
+      objectID: 2,
+    },
+    {
+      title: 'Svelte',
+      url: 'https://svelte.dev/',
+      author: 'Rich Harris',
+      num_comments: 5,
+      points: 5,
+      objectID: 3,
+    },
+  ];

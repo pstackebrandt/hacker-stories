eb34d6a3e40559746fd07747c1bcf5d40cd2b5c4
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2024-12-14 12:46:35 +0100

    Refactor frameworksAndLibs data structure to include detailed comments and use Object.freeze for immutability. This enhances code clarity and ensures the data remains constant throughout the application.

--- src/data/frameworks.js
+++ src/data/frameworks.js
@@ -1,34 +1,42 @@
-export const frameworksAndLibs = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-    {
-      title: 'Vue.js',
-      url: 'https://vuejs.org/',
-      author: 'Evan You',
-      num_comments: 4,
-      points: 3,
-      objectID: 2,
-    },
-    {
-      title: 'Svelte',
-      url: 'https://svelte.dev/',
-      author: 'Rich Harris',
-      num_comments: 5,
-      points: 5,
-      objectID: 3,
-    },
-  ];
+// List of javascript frameworks and libraries
+// Each framework/library has:
+// - title: Name of the framework/library
+// - url: Official website URL
+// - author: Creator(s) of the framework/library
+// - num_comments: Number of comments
+// - points: Rating points
+// - objectID: Unique identifier
+export const frameworksAndLibs = Object.freeze([
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke', 
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2, 
+    points: 5,
+    objectID: 1,
+  },
+  {
+    title: 'Vue.js',
+    url: 'https://vuejs.org/',
+    author: 'Evan You',
+    num_comments: 4,
+    points: 3,
+    objectID: 2,
+  },
+  {
+    title: 'Svelte', 
+    url: 'https://svelte.dev/',
+    author: 'Rich Harris',
+    num_comments: 5,
+    points: 5,
+    objectID: 3,
+  },
+]);
